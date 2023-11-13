e1a8cc4e54d20a91a83b496954f4032d5853b447
elprofenoe <134880729+elprofenoe@users.noreply.github.com>
2023-11-13 12:28:27 -0500

    Actualizar pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,70 +1,37 @@
-function Izquierda () {
-    basic.showArrow(ArrowNames.NorthWest)
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    pins.digitalWritePin(DigitalPin.P8, 0)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-}
-// Aquí haces tu programa
-input.onButtonPressed(Button.A, function () {
-    adelante()
-    basic.pause(500)
-    Girar_Izquierda()
-    basic.pause(2000)
-    Parar()
-    basic.pause(500)
-    atras()
-    basic.pause(500)
-    Girar_Derecha()
-    basic.pause(2000)
-    Parar()
-    basic.pause(100)
-})
-function Parar () {
+namespace Naranbot{
+export function Parar(): void {
     basic.showIcon(IconNames.SmallSquare)
     pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 0)
     pins.digitalWritePin(DigitalPin.P8, 0)
 }
-function adelante () {
+export function adelante(): void {
     basic.showArrow(ArrowNames.North)
     pins.digitalWritePin(DigitalPin.P0, 1)
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 1)
     pins.digitalWritePin(DigitalPin.P8, 0)
 }
-function Derecha () {
-    basic.showArrow(ArrowNames.NorthWest)
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    pins.digitalWritePin(DigitalPin.P2, 1)
-    pins.digitalWritePin(DigitalPin.P8, 0)
-}
-input.onButtonPressed(Button.B, function () {
-    Parar()
-})
-function Girar_Derecha () {
+export function Girar_Derecha(): void {
     basic.showArrow(ArrowNames.East)
     pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 1)
     pins.digitalWritePin(DigitalPin.P8, 0)
 }
-function atras () {
+export function atras(): void {
     basic.showArrow(ArrowNames.South)
     pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 0)
     pins.digitalWritePin(DigitalPin.P8, 1)
 }
-function Girar_Izquierda () {
+export function Girar_Izquierda(): void {
     basic.showArrow(ArrowNames.West)
     pins.digitalWritePin(DigitalPin.P0, 1)
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 0)
     pins.digitalWritePin(DigitalPin.P8, 1)
 }
-Parar()
-basic.showIcon(IconNames.StickFigure)
-basic.pause(200)
+}
